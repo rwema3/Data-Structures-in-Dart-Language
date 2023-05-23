9c017e244491c1b42daf6a4eeaebd906797a2cf3
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-05-23 19:00:31 +0200

    number Of Elements

--- 05_recursion/merge_sort.dart
+++ 05_recursion/merge_sort.dart
@@ -5,4 +5,24 @@ class Array {
   int numberOfElements;
 
   Array(int max) {
+    array = new List<int>(max);
+    numberOfElements = 0;
+  }
+
+  void insert(int value) {
+    array[numberOfElements] = value;
+    numberOfElements ++;
+  }
+
+  void display() {
+    for (int i = 0; i < numberOfElements; i ++) {
+      stdout.write('${array[i]} ');
+    }
+    stdout.writeln();
+  }
+
+  void mergeSort() {
+    List<int> workSpace = new List<int>(numberOfElements);
+    _recursiveMergeSort(workSpace, 0, numberOfElements - 1);
+  }
 
