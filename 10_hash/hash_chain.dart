e4f5c9dc9fc1fb5dc06d34b723203bcb8f2dbd93
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-05-30 00:13:27 +0200

    void display Link

--- 10_hash/hash_chain.dart
+++ 10_hash/hash_chain.dart
@@ -10,3 +10,24 @@ class Link {
   int getKey() {
     return data;
   }
+
+  void displayLink() {
+    stdout.write('$data ');
+  }
+}
+
+class SortedList {
+  Link first;
+
+  SortedList() {
+    first = null;
+  }
+
+  /// Insert in ordered
+  void insert(Link link) {
+    int key = link.getKey();
+    Link previous = null;
+    Link current = first;
+
+    while (current != null && key > current.getKey()) {
+      previous = current;
