3e77e94c4488af2af9db61268fa6cb3f75e14de5
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-05-29 23:36:07 +0200

    cLASS Node

--- 07_binary_tree/binary_tree.dart
+++ 07_binary_tree/binary_tree.dart
@@ -1,2 +1,17 @@
 import 'dart:io';
-i
+import 'dart:collection';
+
+class Node {
+  int iData;
+  double dData;
+  Node leftChild;
+  Node rightChild;
+
+  void displayNode() {
+    stdout.write('{');
+    stdout.write(iData);
+    stdout.write(', ');
+    stdout.write(dData);
+    stdout.write('}');
+  }
+}
