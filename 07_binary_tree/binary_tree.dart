74728396407300e78e6d63ba774a5e87c87a57cf
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-05-29 23:40:07 +0200

    Tree Insert

--- 07_binary_tree/binary_tree.dart
+++ 07_binary_tree/binary_tree.dart
@@ -240,3 +240,13 @@ class Tree {
 void main(List<String> args) {
   Tree tree = new Tree();
 
+  tree.insert(50, 1.5);
+  tree.insert(25, 1.7);
+  tree.insert(75, 1.9);
+  tree.insert(12, 1.5);
+  tree.insert(37, 1.2);
+  tree.insert(43, 1.7);
+  tree.insert(30, 1.5);
+  tree.insert(33, 1.2);
+  tree.insert(87, 1.7);
+  tree.insert(93, 1.5);
