7e4fe65cc833e27b0a070a3897f307f5e064bc11
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-05-31 23:43:18 +0200

    Declarations

--- 13_weighted_graph/path.dart
+++ 13_weighted_graph/path.dart
@@ -21,4 +21,25 @@ class GraphPath {
   final int INFINITY = 1000000;
   List<Vertex> vertexList;
   List<List<int>> adjMat;
+  int nVerts;
+  int nTree;
+  int currentVert;
+  List<DistPar> sPath;
+  int startToCurrent;
+
+  GraphPath() {
+    nTree = 0;
+    currentVert = 0;
+    startToCurrent = 0;
+    nVerts = 0;
+    vertexList = new List<Vertex>(maxVerts);
+    adjMat = new List.generate(maxVerts, (int i) => new List(maxVerts));
+    for(int i = 0; i < maxVerts; i ++) {
+      for(int j = 0; j < maxVerts; j ++) {
+        adjMat[i][j] = INFINITY;
+      }
+    }
+
+    sPath = new List<DistPar>(maxVerts);
+  }
 
