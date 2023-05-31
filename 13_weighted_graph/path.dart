a1655ef8c5d13a954394742c1b02d6f4d360926d
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-05-31 23:43:01 +0200

    class DistPar

--- 13_weighted_graph/path.dart
+++ 13_weighted_graph/path.dart
@@ -5,4 +5,20 @@ class Vertex {
   bool isInTree;
 
   Vertex(this.label) {
+    isInTree = false;
+  }
+}
+
+class DistPar {
+  int distance;
+  int parentVert;
+
+  DistPar(this.parentVert, this.distance);
+}
+
+class GraphPath {
+  final int maxVerts = 20;
+  final int INFINITY = 1000000;
+  List<Vertex> vertexList;
+  List<List<int>> adjMat;
 
